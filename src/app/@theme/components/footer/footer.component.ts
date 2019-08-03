245967ba0d9cce91902541570e39f0b0d8b1456a
Maxime Clement <maxime-clement@live.fr>
2019-08-04 02:07:58 +0300

    home added few stuff

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -9,14 +9,21 @@ import { Component } from '@angular/core';
     >
     <div class="socials">
       <a
-        href="./pages/contacts"
+        href="https://github.com/pseudoincorrect"
         target="_blank"
-        class="ion ion-email ng-star-inserted"
+        class="ion ion-social-github"
+        style="margin-left: 10px;"
       ></a>
       <a
         href="https://www.linkedin.com/in/maximeclement-iot/"
         target="_blank"
         class="ion ion-social-linkedin"
+        style="margin-left: 10px;"
+      ></a>
+      <a
+        href="./pages/contacts"
+        target="_blank"
+        class="ion ion-email"
         style="margin-left: 10px;"
       ></a>
     </div>
--- src/app/pages/contacts/contacts.module.ts
+++ src/app/pages/contacts/contacts.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
 import { ThemeModule } from '../../@theme/theme.module';
-import { NbCardModule } from '@nebular/theme';
+import { NbCardModule, NbIconModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
     ContactsComponent,
   ],
   imports: [
+    NbIconModule,
+    NbEvaIconsModule,
     NbCardModule,
     CommonModule,
     ThemeModule,
