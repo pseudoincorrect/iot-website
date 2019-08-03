f1d6906922e5fffe0255edcf186eb01c57bd4ae9
Maxime Clement <maxime-clement@live.fr>
2019-08-03 15:32:56 +0300

    minor css formating

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,12 +4,22 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Thanks <b><a href="https://akveo.com" target="_blank">Akveo</a></b></span>
+    <span class="created-by"
+      >Thanks <b><a href="https://akveo.com" target="_blank">Akveo</a></b></span
+    >
     <div class="socials">
-      <a href="./pages/contacts" target="_blank" class="ion ion-email ng-star-inserted"></a>
-      <a href="https://www.linkedin.com/in/maximeclement-iot/" target="_blank" class="ion ion-social-linkedin" style="margin-left: 10px;"></a>
+      <a
+        href="./pages/contacts"
+        target="_blank"
+        class="ion ion-email ng-star-inserted"
+      ></a>
+      <a
+        href="https://www.linkedin.com/in/maximeclement-iot/"
+        target="_blank"
+        class="ion ion-social-linkedin"
+        style="margin-left: 10px;"
+      ></a>
     </div>
-  `,
+  `
 })
-export class FooterComponent {
-}
+export class FooterComponent {}
