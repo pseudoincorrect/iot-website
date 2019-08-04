2439c6ead976a9def626db7d1184677e641c71af
Maxime Clement <maxime-clement@live.fr>
2019-08-04 12:11:12 +0300

    pie chart updated (dependencies)

--- src/app/pages/skills/charts/chartjs-pie.component.ts
+++ src/app/pages/skills/charts/chartjs-pie.component.ts
@@ -1,18 +1,12 @@
-import {
-  Component,
-  OnDestroy,
-} from '@angular/core';
-import {
-  NbThemeService,
-} from '@nebular/theme';
-// import { ChartModule } from 'angular2-chartjs';
+import { Component, OnDestroy } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 import 'chartjs-plugin-labels';
 
 @Component({
   selector: 'ngx-chartjs-pie',
   template: `
     <chart type="pie" [data]="data" [options]="options"></chart>
-  `,
+  `
 })
 export class ChartjsPieComponent implements OnDestroy {
   data: any;
@@ -21,16 +15,29 @@ export class ChartjsPieComponent implements OnDestroy {
 
   constructor(private theme: NbThemeService) {
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
-
       const colors: any = config.variables;
       const chartjs: any = config.variables.chartjs;
 
       this.data = {
-        labels: ['Web\n\rTechnologies', '\n\rEmbedded\n\rSystems\n\rFirmware', 'Hardware', 'Project\n\rManag.'],
-        datasets: [{
-          data: [130, 100, 80, 80],
-          backgroundColor: [colors.primaryLight, colors.infoLight, colors.successLight, colors.warning],
-        }],
+        labels: [
+          'Web\n\rTechnologies',
+          '\n\rEmbedded\n\rSystems\n\rFirmware',
+          'Hardware',
+          'Project\n\rManag.'
+        ],
+        datasets: [
+          {
+            data: [130, 100, 80, 80],
+            borderAlign: 'inner',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            borderColor: [
+              colors.primaryLight,
+              colors.infoLight,
+              colors.successLight,
+              colors.warning
+            ]
+          }
+        ]
       };
 
       this.options = {
@@ -39,25 +46,29 @@ export class ChartjsPieComponent implements OnDestroy {
             render: 'label',
             fontSize: 15,
             fontColor: 'white',
-            fontStyle: 'bold',
-          },
+            fontStyle: 'bold'
+          }
         },
         maintainAspectRatio: false,
         responsive: true,
         scales: {
-          xAxes: [{
-            display: false,
-          } ],
-          yAxes: [{
-            display: false,
-          } ],
+          xAxes: [
+            {
+              display: false
+            }
+          ],
+          yAxes: [
+            {
+              display: false
+            }
+          ]
         },
         legend: {
           display: false,
           labels: {
-            fontColor: chartjs.textColor,
-          },
-        },
+            fontColor: chartjs.textColor
+          }
+        }
       };
     });
   }
