8cfa7bcd61bb086ef5be56f4861588fbbd144da2
Maxime Clement <maxime-clement@live.fr>
2019-08-04 23:08:09 +0300

    contact clipboard

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
 import { CollapseSidebarService } from '../../@core/utils';
+import { NbToastrService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-contacts',
@@ -13,7 +14,8 @@ export class ContactsComponent implements OnInit, OnDestroy {
 
   constructor(
     public collapseSidebarService: CollapseSidebarService,
-    private themeService: NbThemeService
+    private themeService: NbThemeService,
+    private toastrService: NbToastrService
   ) {
     this.themeSubscription = this.themeService.getJsTheme().subscribe(theme => {
       this.currentTheme = theme.name;
@@ -24,5 +26,27 @@ export class ContactsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.themeSubscription.unsubscribe();
+  }
+
+  showToast(position, status) {
+    this.toastrService.show(
+      '',
+      `Email copied to clipboard`,
+      { position, status });
+  }
+
+  copyMessage(val: string){
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.showToast('top-right', 'success');
   }
 }
--- src/app/pages/contacts/contacts.module.ts
+++ src/app/pages/contacts/contacts.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
 import { ThemeModule } from '../../@theme/theme.module';
-import { NbCardModule, NbIconModule } from '@nebular/theme';
+import { NbCardModule, NbIconModule, NbButtonModule, NbToastrModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
@@ -10,11 +10,13 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     ContactsComponent,
   ],
   imports: [
+    NbButtonModule,
     NbIconModule,
     NbEvaIconsModule,
     NbCardModule,
     CommonModule,
     ThemeModule,
+    NbToastrModule.forRoot(),
   ],
 })
 export class ContactsModule { }
